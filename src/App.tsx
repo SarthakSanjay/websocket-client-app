ef41dd4b489fe137885bd2e8c9204cfea3782cb3
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-29 23:59:46 +0530

    ui change

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 
 function App() {
   const [socket, setSocket] = useState<null | WebSocket>(null)
@@ -23,16 +22,32 @@ function App() {
       setAllMessage(m => [...m , message.data])
     }
     setSocket(socket)
+
+    return () =>{
+      socket.close()
+    }
   },[])
   if(!socket){
     return <div>connecting to ws server...</div>
   }
   return (
-    <div style={{height:'100vh',width:'100vw'}}>
-      <input onChange={handleChange}/>
+    <div style={{height:'100vh',width:'100vw' }}>
+     <div style={{position:'absolute' , bottom:'50px',height:'40px' , width:'100vw', display:'flex',justifyContent:"center" ,alignItems:'center'}}>
+     <input onChange={handleChange}
+      style={{height:'100%', width:'20vw', borderRadius:'20px',outline:'none' , border:"2px solid white",paddingLeft:"30px",
+    borderTopRightRadius:"0px",borderBottomRightRadius:"0px"}}
+     />
       <button onClick={()=>{
         socket.send(text)
+      }}
+      style={{
+        height:'100%' ,
+        background:"skyblue",
+        color:'black',
+        borderTopLeftRadius:'0px',
+        borderBottomLeftRadius:'0px',
       }}>send</button>
+     </div>
       {messages}
       {allMessage.map((msg) =>{
         return <p>{msg}</p>
