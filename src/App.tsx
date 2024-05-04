bd761a21718969d480e7957fe4e316df4c6aaa69
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-04 23:14:33 +0530

    Added msg input and outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from 'react'
+import { Outlet, useParams } from 'react-router-dom'
+import MessageInputBar from './components/MessageInputBar'
 
 function App() {
   const [socket, setSocket] = useState<null | WebSocket>(null)
   const [messages , setMessages] = useState('')
   const [text ,setText] = useState<string>('')
   const [allMessage , setAllMessage] = useState([])
-
+  const {username} = useParams()
+  console.log(username);
   const handleChange = (e:any) =>{
     setText(e.target.value)
   }
   useEffect(()=>{
-    const socket = new WebSocket('ws://localhost:8080')
+    console.log(username);
+    const socket = new WebSocket(`ws://localhost:8080`)
     socket.onopen = () =>{
       console.log('connected');
       setSocket(socket)
     }
     socket.onmessage = (message) =>{
-      console.log('received message' , message.data);
-      console.log(message);
+      // console.log('received message' , message.data);
+      console.log('message',message);
       setMessages(message.data)
+      //@ts-ignore
       setAllMessage(m => [...m , message.data])
     }
     setSocket(socket)
@@ -31,27 +36,11 @@ function App() {
     return <div>connecting to ws server...</div>
   }
   return (
-    <div style={{height:'100vh',width:'100vw' }}>
-     <div style={{position:'absolute' , bottom:'50px',height:'40px' , width:'100vw', display:'flex',justifyContent:"center" ,alignItems:'center'}}>
-     <input onChange={handleChange}
-      style={{height:'100%', width:'20vw', borderRadius:'20px',outline:'none' , border:"2px solid white",paddingLeft:"30px",
-    borderTopRightRadius:"0px",borderBottomRightRadius:"0px"}}
-     />
-      <button onClick={()=>{
-        socket.send(text)
-      }}
-      style={{
-        height:'100%' ,
-        background:"skyblue",
-        color:'black',
-        borderTopLeftRadius:'0px',
-        borderBottomLeftRadius:'0px',
-      }}>send</button>
-     </div>
-      {messages}
-      {allMessage.map((msg) =>{
-        return <p>{msg}</p>
-      })}
+    <div className='h-screen w-screen bg-black text-white flex justify-center'>
+      <Outlet />
+      <div className='w-full absolute bottom-10 flex justify-center'>
+        <MessageInputBar />
+      </div>
     </div>
   )
 }
