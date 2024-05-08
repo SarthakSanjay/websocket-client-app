156a4511a889373728ab2a70e61d71a2eab97b65
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-08 22:56:47 +0530

    Implemented socket logic

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,12 +1,48 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
 
 const ChatWindow = () => {
-    const {username} = useParams()
-    useEffect(()=>{},[])
+    const {connectionId} = useParams()
+    const [searchParams ,setSearchParams] = useSearchParams()
+    const [socket, setSocket] = useState<null | WebSocket>(null)
+    const [messages , setMessages] = useState('')
+    // const [text ,setText] = useState<string>('')
+    const [allMessage , setAllMessage] = useState([])
+    // console.log(username);
+    // const handleChange = (e:any) =>{
+    //   setText(e.target.value)
+    // }
+    console.log('username',searchParams.get('username'));
+    useEffect(()=>{
+      const username = searchParams.get('username')
+      const socket = new WebSocket(`ws://localhost:8080/${connectionId}?username=${username}`)
+      socket.onopen = () =>{
+        console.log('connected');
+        setSocket(socket)
+      }
+      socket.onmessage = (message) =>{
+        // console.log('received message' , message.data);
+        console.log('message',message);
+        setMessages(message.data)
+        //@ts-ignore
+        setAllMessage(m => [...m , message.data])
+      }
+      setSocket(socket)
+  
+      return () =>{
+        socket.close()
+      }
+    },[])
+    console.log(allMessage);
+    if(!socket){
+      return <div>connecting to ws server...</div>
+    }
   return (
     <div className='w-3/4 h-[85vh] border m-4'>
-        {username}
+
+        {allMessage.map((msg,i)=>{
+          return <div key={i}>{msg}</div>
+        })}
     </div>
   )
 }
