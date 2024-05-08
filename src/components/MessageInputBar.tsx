0a35bbcd24c13c341c0c063df0d619e10a831dec
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-08 22:57:20 +0530

    Implemented socket logic for sending message

--- src/components/MessageInputBar.tsx
+++ src/components/MessageInputBar.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
 
 const MessageInputBar = () => {
+  const [text , setText] = useState('')
+  const [searchParams , setSearchParams] = useSearchParams()
+  const [connection, setConnection] = useState<null | WebSocket>(null);
+  const {connectionId} = useParams()
+  const handleChange = (e:any) =>{
+    setText(e.target.value)
+  }
+  useEffect(()=>{
+    const username = searchParams.get('username')
+      const socket = new WebSocket(`ws://localhost:8080/${connectionId}?username=${username}`)
+      socket.onopen = () =>{
+        setConnection(socket)
+      }
+
+      return () =>{
+        socket.close()
+      }
+  },[])
+  const handleClick = () =>{
+    if (connection && text) {
+      connection.send(text);
+      setText(''); 
+    }
+  }
   return (
     <div className='h-15 w-1/2 flex justify-center items-center'>
-        <input className='h-full w-[600px] px-5'/>
-        <button className='h-full w-[200px]'>Send</button>
+        <input className='h-full w-[600px] px-5' 
+        onChange={handleChange}/>
+        <button className='h-full w-[200px]'
+        onClick={handleClick}>Send</button>
     </div>
   )
 }
