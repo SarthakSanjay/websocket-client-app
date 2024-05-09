120b042bff4b6ca68e2f0fb3d1773664ec4f52ca
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-09 23:08:05 +0530

    modified app

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
-import { useEffect, useState } from 'react'
-import { Outlet, useParams } from 'react-router-dom'
-import MessageInputBar from './components/MessageInputBar'
+import MessageArea from './components/MessageArea'
+import Sidebar from './components/Sidebar'
 
 function App() {
-  const [socket, setSocket] = useState<null | WebSocket>(null)
-  const [messages , setMessages] = useState('')
-  const [text ,setText] = useState<string>('')
-  const [allMessage , setAllMessage] = useState([])
-  const {username} = useParams()
-  console.log(username);
-  const handleChange = (e:any) =>{
-    setText(e.target.value)
-  }
-  useEffect(()=>{
-    console.log(username);
-    const socket = new WebSocket(`ws://localhost:8080`)
-    socket.onopen = () =>{
-      console.log('connected');
-      setSocket(socket)
-    }
-    socket.onmessage = (message) =>{
-      // console.log('received message' , message.data);
-      console.log('message',message);
-      setMessages(message.data)
-      //@ts-ignore
-      setAllMessage(m => [...m , message.data])
-    }
-    setSocket(socket)
 
-    return () =>{
-      socket.close()
-    }
-  },[])
-  if(!socket){
-    return <div>connecting to ws server...</div>
-  }
   return (
-    <div className='h-screen w-screen bg-black text-white flex justify-center'>
-      <Outlet />
-      <div className='w-full absolute bottom-10 flex justify-center'>
-        <MessageInputBar />
-      </div>
+    <div className='h-screen w-screen bg-black text-white flex'>
+        <Sidebar />
+        <MessageArea />
     </div>
   )
 }
