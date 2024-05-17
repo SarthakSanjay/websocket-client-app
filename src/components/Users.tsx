67fd519f711ede3eb605bcb68af1cae7bc455318
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-17 23:49:31 +0530

    added Users layout

---
+++ src/components/Users.tsx
@@ -0,0 +1,15 @@
+import UserChat from "./UserChat"
+
+const Users = () => {
+  return (
+    <div className="h-[95%] w-full ">
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
+        <UserChat />
+    </div>
+  )
+}
+
+export default Users
