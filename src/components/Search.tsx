e60aea76ef64df5e15479c4990545586595a4a01
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-17 23:48:12 +0530

    added search bar

---
+++ src/components/Search.tsx
@@ -0,0 +1,11 @@
+const Search = () => {
+  return (
+    <div className="h-10 w-full bg-purple-300 text-black rounded-lg">
+        <input type="text" className="h-full w-full bg-transparent outline-none px-3 text-black
+        placeholder:text-black "
+        placeholder="search" />
+    </div>
+  )
+}
+
+export default Search
