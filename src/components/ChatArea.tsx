ece1be7ec8cd1d173c9555b971fb84bb2ee2cffe
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-17 23:47:21 +0530

    added chat area layout

---
+++ src/components/ChatArea.tsx
@@ -0,0 +1,11 @@
+import ChatAreaSidebar from "./ChatAreaSidebar"
+
+const ChatArea = () => {
+  return (
+    <div className="h-full w-[65%] rounded-3xl bg-purple-100 p-4">
+        <ChatAreaSidebar />
+    </div>
+  )
+}
+
+export default ChatArea
