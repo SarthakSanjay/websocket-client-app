b8d5f8f1e68d4aee21ee348cad74daa3acdfeebd
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-18 23:31:08 +0530

    initialized user detail component

---
+++ src/components/UserDetails.tsx
@@ -0,0 +1,9 @@
+const UserDetails = () => {
+  return (
+    <div className="h-1/2 w-[30%] bg-purple-200 rounded-3xl p-3">
+        <text className="text-black text-2xl p-2">Username</text>
+    </div>
+  )
+}
+
+export default UserDetails
