a65b5415ca955187e1fe7129e427244f490c6144
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-18 23:36:51 +0530

    added outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import MessageArea from './components/MessageArea'
-import Sidebar from './components/Sidebar'
+import { Outlet } from "react-router-dom"
+import Sidebar from "./components/Sidebar"
+import UserDetails from "./components/UserDetails"
 
 function App() {
 
   return (
-    <div className='h-screen w-screen bg-black text-white flex'>
+    <div className='h-screen w-screen bg-black text-white flex gap-2 p-2'>
         <Sidebar />
-        <MessageArea />
+        <Outlet />
+        <UserDetails />
     </div>
   )
 }
