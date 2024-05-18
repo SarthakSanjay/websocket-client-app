59a462153326ea1bdb16898d9709de233ee9d26f
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-18 23:30:00 +0530

    added search icon

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,10 @@
+import { FiSearch } from "react-icons/fi";
+
 const Search = () => {
   return (
-    <div className="h-10 w-full bg-purple-300 text-black rounded-lg">
-        <input type="text" className="h-full w-full bg-transparent outline-none px-3 text-black
+    <div className="h-10 w-full bg-purple-300 flex text-black rounded-lg">
+        <div className="h-10 w-10 text-2xl flex justify-center items-center"><FiSearch /></div>
+        <input type="text" className="h-full w-full bg-transparent outline-none px-1 text-black
         placeholder:text-black "
         placeholder="search" />
     </div>
