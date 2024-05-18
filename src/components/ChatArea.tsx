277edb136809a9688dbce8194e81e0effa8c6c95
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-18 23:31:42 +0530

    modified

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -1,9 +1,11 @@
 import ChatAreaSidebar from "./ChatAreaSidebar"
+import Chats from "./Chats"
 
 const ChatArea = () => {
   return (
-    <div className="h-full w-[65%] rounded-3xl bg-purple-100 p-4">
+    <div className={`h-full w-[65%] flex rounded-3xl bg-purple-100 p-4`}>
         <ChatAreaSidebar />
+        <Chats />
     </div>
   )
 }
