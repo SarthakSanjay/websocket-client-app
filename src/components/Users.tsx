8d6c7061cbe22e013894b71631976e83cfc347fa
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-24 23:46:59 +0530

    added get friends http request

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react"
 import UserChat from "./UserChat"
+import axios from "axios"
+import { TOKEN } from "../utils/util"
 
 const Users = () => {
+  const [friends , setFriends] = useState([])
+  console.log('friends',friends);
+  useEffect(()=>{
+    async function getFriends() {
+      const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/friend/all`,{
+        headers: {
+          Authorization: `Bearer ${TOKEN}`
+        }
+      })
+      setFriends(res.data.friends)
+    }
+    getFriends()
+  },[])
   return (
     <div className="h-[95%] w-full ">
         <UserChat />
