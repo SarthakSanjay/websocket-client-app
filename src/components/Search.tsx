b101f09d3e54d2f75cc17216ec1af3a60b682db8
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-27 23:49:42 +0530

    refactored

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import SearchModal from "./SearchModal";
+import axios from "axios";
+import { TOKEN } from "../utils/util";
 
 const Search = () => {
+  const [input , setInput] = useState('')
+  const [users ,setUsers] = useState([])
+  useEffect(()=>{
+    
+    axios.get(`${import.meta.env.VITE_BASE_URL}/user/search`,{
+      params:{
+        search: input
+      },
+      headers:{
+        Authorization: `Bearer ${TOKEN}`
+      }
+    }).then(res => setUsers(res.data.users))
+    console.log(users);
+  },[input])
   return (
-    <div className="h-10 w-full bg-purple-300 flex text-black rounded-lg">
-        <div className="h-10 w-10 text-2xl flex justify-center items-center"><FiSearch /></div>
-        <input type="text" className="h-full w-full bg-transparent outline-none px-1 text-black
-        placeholder:text-black "
+    <div className="h-10 w-full bg-white/15 border border-black flex text-white rounded-lg ">
+        <div className="h-10 w-10 text-2xl flex justify-center items-center text-black"><FiSearch /></div>
+        <input type="text" className="h-full w-full bg-transparent outline-none px-1
+        placeholder:text-black"
+        onChange={(e)=>{
+          setInput(e.target.value)
+        }}
         placeholder="search" />
+        {input.trim() !== '' && 
+        
+        <SearchModal users={users} />
+        }
     </div>
   )
 }
