172cc3e212c32f4836149dc015e1613902265249
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-01 23:40:38 +0530

    modified FriendReq component

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -7,7 +7,7 @@ import { useSetRecoilState } from 'recoil'
 
 interface request {
     id: number
-    friend: User
+    sender: User
     date :Date
 }
 const FriendRequests = () => {
@@ -28,7 +28,7 @@ const FriendRequests = () => {
         fetchRequests()
     },[])
   return (
-    <div className={`h-full w-[65%] flex rounded-3xl bg-gradient-to-r from-blue-600 to-violet-600 dark:bg-purple-950  p-4`}>
+    <div className={`h-full w-[65%] flex flex-col gap-2 rounded-3xl bg-gradient-to-r from-blue-600 to-violet-600 dark:bg-purple-950  p-4`}>
         {requests.map((req:request) => {
             return <Request req={req} />
         })}
@@ -39,15 +39,32 @@ interface reqProp {
     req: request
 }
 const Request: React.FC<reqProp> = ({req}) =>{
+    const [isAccepted ,setIsAccepted] = useState(false)
     return <div className='bg-white/35 w-2/3 h-14 rounded-lg text-black px-3 flex items-center justify-between'>
     <div className='flex gap-3 items-center'>
         <img src='https://www.github.com/sarthaksanjay.png' 
         className='h-10 w-10 rounded-lg' />
-        <h1>{req.friend.username}</h1>
+      <div className='h-10 flex flex-col '>
+      <h1 className='text-lg w-max h-5'>{req.sender.username}</h1>
+        <span className='text-sm h-5 w-max text-gray-500'>send you friend request</span>
+      </div>
     </div>
     <div className='flex gap-3'>
-        <button className='bg-black/60 text-green-500 h-7 w-max px-2 rounded-lg'>Accept</button>
-        <button className='bg-black/60 text-red-500 h-7 w-max px-2 rounded-lg'>Reject</button>
+        <button 
+        className={`bg-black/80 ${isAccepted ? "text-green-500" : "text-sky-500"} h-7 w-max px-2 rounded-lg`}
+        onClick={async()=>{
+            const res = await axios.put(`${import.meta.env.VITE_BASE_URL}/friend/accept`,{
+                friendId: req.sender.id
+            },{
+                headers:{
+                    Authorization: `Bearer ${TOKEN}`
+                }
+            })
+            if(res.data.Accepted){
+                setIsAccepted(true)
+            }
+        }}>{isAccepted ? 'Accepted' : 'Accept'}</button>
+        <button className='bg-black/80 text-orange-500 h-7 w-max px-2 rounded-lg'>Reject</button>
     </div>
 </div>
 }
