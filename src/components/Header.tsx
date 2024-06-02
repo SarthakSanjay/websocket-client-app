d9a9f0c308385ac7fb119e5c741d545fdc66ba53
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-02 23:09:35 +0530

    added user prop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,24 @@
 import { FiSearch } from "react-icons/fi"
 import { CiMenuKebab } from "react-icons/ci";
+import { User } from "./SearchModal";
 
-const Header = () => {
+interface HeaderProp {
+  user : User
+}
+const Header : React.FC<HeaderProp> = ({user}) => {
   return (
     <div className="h-14 w-full px-6 flex items-center justify-between">
-        <text className="font-semibold text-black text-2xl">Username</text>
+        <span className="font-semibold text-black text-2xl">{user.username.charAt(0).toUpperCase()+ user.username.slice(1)}</span>
        <div className="flex">
        <div
           className="h-14 w-14 text-2xl flex items-center
-         justify-center text-black rounded-full hover:bg-purple-300"
+         justify-center text-black rounded-full hover:bg-black/35 hover:text-white transition-all duration-500"
         >
           <FiSearch />
         </div>
         <div
           className="h-14 w-14 text-2xl flex items-center
-         justify-center text-black rounded-full hover:bg-purple-300"
+         justify-center text-black rounded-full  hover:bg-black/35 hover:text-white transition-all duration-500"
         >
           <CiMenuKebab />
         </div>
