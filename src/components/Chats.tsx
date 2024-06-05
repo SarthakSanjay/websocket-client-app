c0b8ddca4dacbb3f1de04ddee598e93ff45db41e
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-05 23:36:31 +0530

    added websocket client code

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -14,17 +14,21 @@ interface Message {
   text: string;
 }
 
+interface SavedMessage extends Message {
+  id:number,
+  senderId:number,
+  receiverId:number,
+}
 
 const Chats = () => {
   const {id} = useParams()
   const [user , setUser] = useState<User>({id:0,username:'',email:''})
   const [message , setMessages] = useState<Message[]>([])
-  // const messages = useRecoilValue(messageAtom)
+  const [savedMessages , setSavedMessages] = useState([])
   const ws = useRef<WebSocket | null>(null);
-  
   const userLoadable = useRecoilValueLoadable(fetchSelfDetails);
   const [myId, setMyId] = useState<number>(0);
-  console.log('message',message);
+
   useEffect(() => {
     if (userLoadable.state === 'hasValue') {
       setMyId(userLoadable.contents.id);
@@ -44,29 +48,42 @@ const Chats = () => {
     }
     fetchUserDetails()
 
-     
+     async function fetchChatMessages() {
+      const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/message/chat/${id}`,{
+          headers:{
+            Authorization: `Bearer ${TOKEN}`
+          }
+      })
+      setSavedMessages(res.data.messages)
+     }
 
+     fetchChatMessages()
+     console.log('id changed');
   },[id])
 
-  useEffect(()=>{
-    ws.current = new WebSocket(`ws://localhost:8080/${myId}`)
+  useEffect(() => {
+    if (myId) {
+      ws.current = new WebSocket(`ws://localhost:8080/${myId}`);
 
-    ws.current.onopen = () =>{
-      console.log('connected to ws server')
-    }
-    ws.current.onmessage = (e) =>{
-      const receivedMessage = JSON.parse(e.data);
-      setMessages((prevMessages) => [...prevMessages, receivedMessage]);
-    }
+      ws.current.onopen = () => {
+        console.log('Connected to WebSocket server');
+      };
+      
+      ws.current.onmessage = (e) => {
+        const receivedMessage  = JSON.parse(e.data);
+        setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+      };
 
-    ws.current.onclose = () =>{
-      console.log(`Ws disconneted`);
-    }
+      ws.current.onclose = () => {
+        console.log('WebSocket disconnected');
+      };
 
-    return () =>{
-      ws.current?.close()
+      return () => {
+        ws.current?.close();
+      };
     }
-  },[user.id ,ws])
+  }, [user.id]);
+
 
   const handleSendMessage = (message: Message) => {
     setMessages((prevMessages) => [...prevMessages, message]);
@@ -75,10 +92,12 @@ const Chats = () => {
   return (
     <div className="h-full w-[70%] relative text-black">
       <Header user={user} />
-      <div className="h-[80%] px-3 overflow-y-scroll flex flex-col">
-        {/* <Message text="hello" /> */}
+      <div className="h-[80%] px-3 overflow-y-scroll flex flex-col" >
+        {
+          savedMessages.map((msg:SavedMessage)=>{
+            return <Message key={msg.id} text={msg.text} self={msg.senderId === myId ? true : false} />
+           })}
        {message.map((msg,index)=>{
-        console.log(msg.text);
         return <Message key={index} text={msg.text} self={msg.friendId !== myId ? true : false} />
        })}
       </div>
