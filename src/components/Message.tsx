de6876b8a5a7dfe64e76b127f1490eb9c46a35d0
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-05 23:31:51 +0530

    refactored message component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,20 +1,19 @@
-import '../index.css'
 interface MessageProp {
-  self ?: boolean
-  text : string
+  self?: boolean;
+  text: string;
 }
-const Message : React.FC<MessageProp> = ({self,text}) => {
-  console.log(self);
+const Message: React.FC<MessageProp> = ({ self, text }) => {
+  // console.log(self);
   return (
-    <div className={`h-min  max-w-[40%] py-2 px-3 rounded-lg m-3 relative text-purple-950
-    ${self
-       ? ' bg-purple-400 before:before-custom before:bg-custom-gradient self-end'
-    :' bg-purple-300 before:my before:my-custom-gradient'} 
+    <div
+      className={`h-min  max-w-[40%] py-2 px-3 rounded-lg m-3 relative text-purple-950
+    ${self ? " bg-black/35 text-white self-end" : " bg-white/35 self-start"} 
 
-    `}>
-        {text}
+    `}
+    >
+      {text}
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
