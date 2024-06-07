ee4aae99e779706c9729ddd224dd4ca5bfe77282
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-07 23:49:47 +0530

    added fetch user details req

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,7 +1,28 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { TOKEN } from "../utils/util"
+import { User } from "./SearchModal"
+
 const UserDetails = () => {
+  const [user, setUser] = useState<User>({id:0,username:'',email:''})
+  const {id} = useParams()
+
+  useEffect(()=>{
+    async function fetchUserDetails() {
+      const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/user/${id}`,{
+        headers:{
+          Authorization: `Bearer ${TOKEN}`
+        }
+      })
+      setUser(res.data.user)
+
+    }
+    fetchUserDetails()
+  },[])
   return (
-    <div className="h-1/2 w-[30%] bg-purple-200 rounded-3xl p-3">
-        <text className="text-black text-2xl p-2">Username</text>
+    <div className="h-1/2 w-[30%]  bg-gradient-to-r from-fuchsia-600 to-purple-600 rounded-3xl p-3">
+        <span className="text-black text-2xl p-2">{user.username}</span>
     </div>
   )
 }
