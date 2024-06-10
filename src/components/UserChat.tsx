6cf94f0fd7bfd9eeb37009b404692e840db590dc
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-10 22:43:07 +0530

    added active userfunc

--- src/components/UserChat.tsx
+++ src/components/UserChat.tsx
@@ -1,13 +1,34 @@
-const UserChat = () => {
+import React, { useEffect, useState } from "react"
+import { User } from "./SearchModal"
+import { Link, useParams } from "react-router-dom"
+import { convertToUppercase } from "../utils/util"
+
+interface UserChatProp {
+  user: User
+}
+const UserChat :React.FC<UserChatProp> = ({user}) => {
+  const [active , setActive] = useState(false)
+  const {id} = useParams()
+  useEffect(()=>{
+    if(id === user.id.toString()){
+      setActive(true)
+    }
+
+    return () =>{
+      setActive(false)
+    }
+  },[id , user.id])
   return (
-    <div className='h-16 w-full hover:bg-purple-200 rounded-lg flex mt-2 justify-between items-center'>
+   <Link to={`/home/user/${user.id}`}>
+       <div className={`h-16 w-full hover:bg-white/15 rounded-lg flex mt-2 gap-2 justify-between items-center ${active ? 'bg-white/15' : ''}`}>
         <img src='https://www.github.com/sarthaksanjay.png' 
-        className='h-16 w-16 rounded-lg' />
-        <div className='h-16 w-[70%] text-black flex flex-col '>
-            <text className='font-semibold'>name</text>
-            <text className='font-extralight'>message</text>
+        className='h-14 w-14 rounded-lg ml-1' />
+        <div className='h-16 w-[70%] text-black hover:text-white flex flex-col justify-center'>
+            <span className='font-semibold'>{convertToUppercase(user.username)}</span>
+            <span className='font-extralight'>message</span>
         </div>
     </div>
+   </Link>
   )
 }
 
