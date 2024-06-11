4f0ef4929242fdb378e8ecc733c1ddb1004a2c4c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-11 23:31:08 +0530

    refactored

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -1,11 +1,13 @@
+import { Outlet, useParams } from "react-router-dom"
 import ChatAreaSidebar from "./ChatAreaSidebar"
-import Chats from "./Chats"
 
 const ChatArea = () => {
+  const {id} = useParams()
   return (
-    <div className={`h-full w-[65%] flex rounded-3xl bg-purple-100 p-4`}>
+    <div className={`h-full w-[65%] flex rounded-3xl bg-gradient-to-r from-blue-600 to-violet-600 dark:bg-purple-950  p-4`}>
         <ChatAreaSidebar />
-        <Chats />
+        {id ?  <Outlet /> : <div className="self-center mx-auto">Open Chats</div>}
+       
     </div>
   )
 }
