54364497c957623f2ab5a4bf3248b52f0dd0269e
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-11 23:35:40 +0530

    added check to ensure user is logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import Sidebar from "./components/Sidebar"
 import UserDetails from "./components/UserDetails"
+import { TOKEN } from "./utils/util";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isAuth , setIsAuth] = useState(false)
+  const navigate = useNavigate()
 
-  return (
-    <div className='h-screen w-screen bg-black text-white flex gap-2 p-2'>
-        <Sidebar />
-        <Outlet />
-        <UserDetails />
-    </div>
-  )
+  useEffect(()=>{
+    if(TOKEN){
+      setIsAuth(true)
+    }else{
+      setIsAuth(false)
+    }
+
+  },[isAuth])
+  
+  if(!isAuth){
+    navigate('/login')
+  }
+
+  return  <div className='h-screen w-screen bg-black text-white flex gap-2 p-2'>
+  <Sidebar />
+  <Outlet />
+  <UserDetails />
+</div>
 }
 
 export default App
