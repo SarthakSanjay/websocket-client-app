23f6ea9a62548a95de3c87c3fcf341eeb5239708
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-21 23:58:03 +0530

    add: active icon logic

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,14 +2,15 @@ import { BiBell } from "react-icons/bi";
 import { GoPerson } from "react-icons/go";
 import { IoSettingsOutline } from "react-icons/io5";
 import { TbLogout2 } from "react-icons/tb";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useRecoilState, useRecoilValueLoadable } from "recoil";
 import { fetchNotificationCount, totalFriendReqAtom } from "../atom/atom";
 import { useEffect } from "react";
 import { CiChat1 } from "react-icons/ci";
+import Cookies from "js-cookie";
 
 interface SidebarIconProp {
-    url : string
+    url ?: string
     icon : React.ReactNode
     text : string
     value ?: number
@@ -35,16 +36,29 @@ const Sidebar = () => {
         <SidebarIcon icon={<GoPerson />} text="Account" url="account"/>
         <SidebarIcon icon={<BiBell />} text="Notification" url="notification" value={notificationCount}/>
         <SidebarIcon icon={<IoSettingsOutline />} text="Setting" url="setting"/>
-        <SidebarIcon icon={<TbLogout2 />} text="Logout" url="logout"/>
+        {/* work in progress */}
+        <button onClick={()=>{
+          Cookies.remove('token')
+        }}>
+
+        <SidebarIcon icon={<TbLogout2 />} text="Logout" />
+        </button>
     </div>
   );
 };
 
 const SidebarIcon : React.FC<SidebarIconProp>= ({icon,text,url,value}) =>{
-    return  <Link to={`/${url}`}>
+  const location = useLocation()
+  let isActive = location.pathname.includes(url || '')
+
+  if(!url){
+    isActive = false
+  }
+
+    return  <Link to={`/${url === '' ? '' : url}`}>
     <div
-      className="h-24 w-24 text-2xl rounded-lg flex justify-center items-center flex-col
-    hover:bg-gray-800 relative"
+      className={`h-24 w-24 text-2xl mb-1 rounded-lg flex justify-center items-center flex-col
+    hover:bg-gray-800 relative ${isActive ? 'bg-gray-800' :''}`}
     >
       {icon}
       <span className="text-sm">{text}</span>
