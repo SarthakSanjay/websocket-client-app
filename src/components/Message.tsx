aa7aac69384a4d5be5373dfb661966914496d94e
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-21 23:48:21 +0530

    add: conditional rendering message a/c to types

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,12 +1,31 @@
 import { format } from "date-fns";
+import AudioPlayer from "./AudioPlayer";
 
 interface MessageProp {
   self?: boolean;
-  text: string;
+  data ?: string;
   time?: Date;
+  type: string
 }
-const Message: React.FC<MessageProp> = ({ self, text ,time}) => {
-  // console.log(self);
+
+const Message: React.FC<MessageProp> = ({ self, data ,time , type}) => {
+// console.log(self,type,data,time);
+    const renderMessageContent = () => {
+      switch (type) {
+        case 'TEXT':
+          return <p>{data}</p>;
+        case 'VIDEO':
+          return <video className="h-[200px] w-[200px] border" controls src={data}></video>;
+        case 'IMAGE':
+          return <img className="h-[200px] w-[200px]" src={data} alt="Sent image" />;
+        case 'AUDIO':
+          //@ts-ignore
+          return <AudioPlayer src={data} />;
+        default:
+          return null;
+      }
+    };
+
   return (
     <div
       className={`h-min  max-w-[40%] py-2 px-3 rounded-lg m-3 relative text-purple-950
@@ -14,8 +33,8 @@ const Message: React.FC<MessageProp> = ({ self, text ,time}) => {
 
     `}
     >
-      {text}
-      <span className={`text-[10px] absolute top-[100%] ${self ? "left-0" : 'right-0'}`}>{time && format(time,"p")}</span>
+      {renderMessageContent()}
+      <span className={`text-[10px] w-max absolute top-[100%] ${self ? "left-0" : 'right-0'}`}>{time && format(time,"p")}</span>
     </div>
   );
 };
