748d860510ee166ad70fe290899db806db5dc86d
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-23 23:22:00 +0530

    fix: send message a/c to msg type

--- src/components/MessageTypingBar.tsx
+++ src/components/MessageTypingBar.tsx
@@ -8,7 +8,7 @@ import { useRecoilState } from "recoil";
 import axios from "axios";
 import { TOKEN } from "../utils/util";
 
- export interface MessageProp {
+export interface MessageProp {
   friendId: number;
   data?: string;
   messageType: string;
@@ -28,44 +28,50 @@ const MessageTypingBar: React.FC<MessageTypingBarProp> = ({
 }) => {
   const { id } = useParams<{ id: string }>();
   const [input, setInput] = useState("");
-  const [messageType, setMessageType] = useState('TEXT');
+  const [messageType, setMessageType] = useState("TEXT");
   const [fileUrl, setFileUrl] = useRecoilState(fileUrlAtom);
-  const [fileData, setFileData] = useState<{name:string , type:string}>({name:'',type:''});
-  const [file , setFile] = useState(null)
+  const [fileData, setFileData] = useState<{ name: string; type: string }>({
+    name: "",
+    type: "",
+  });
+  const [file, setFile] = useState(null);
   const fileInputRef = useRef(null);
-  const [filename , setFilename] = useState(`image-${Date.now()}`)
+  const [filename, setFilename] = useState(`${Date.now()}`);
 
-  async function uploadFile(file:any){
-    const fileExtension = file.type.split('/')[1];
-    const fileName = `${filename}.${fileExtension}`;
-      
+  async function uploadFile(file: any) {
+    const fileExtension = file.type.split("/")[1];
+    const fileType = file.type.split("/")[0];
+    const fileName = `${fileType}-${filename}.${fileExtension}`;
+
     try {
-      let res = await  axios.post(`/upload/file`, {
-        filename:  fileName,
-        contentType : file.type
-      },
-      {
-        headers:{
-          Authorization: `Bearer ${TOKEN}`
+      let res = await axios.post(
+        `/upload/file`,
+        {
+          filename: fileName,
+          contentType: file.type,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${TOKEN}`,
+          },
         }
-      })
-      if(res.status === 200){
-        console.log(res.status , res.data.url);
-        console.log('formdata',file);
-      let upload = await  axios.put(res.data.url , file , {
-        headers:{
-          "Content-Type" : file.type
-        }
-      })
+      );
+      if (res.status === 200) {
+        console.log(res.status, res.data.url);
+        console.log("formdata", file);
+        let upload = await axios.put(res.data.url, file, {
+          headers: {
+            "Content-Type": file.type,
+          },
+        });
 
-      if(upload.status === 200 ){
-        alert(`image uploaded ${upload.status} ${upload}`)
+        if (upload.status === 200) {
+          alert(`image uploaded ${upload.status} ${upload}`);
+        }
       }
-      
-      }
-     } catch (error) {
-      console.log(error)
-     }
+    } catch (error) {
+      console.log(error);
+    }
   }
   const handleDivClick = () => {
     if (fileInputRef.current) {
@@ -76,14 +82,13 @@ const MessageTypingBar: React.FC<MessageTypingBarProp> = ({
 
   const handleFileChange = (event: any) => {
     const file = event.target.files[0];
-    console.log('file',file.type.split('/')[1]);
-    setFile(file)
+    console.log("file", file.type.split("/")[1]);
+    setFile(file);
     if (file) {
       const url = URL.createObjectURL(file);
       setFileUrl(url);
       setFileData({ name: file.name, type: file.type });
-      setMessageType(file.type.split('/')[0].toUpperCase())
-
+      setMessageType(file.type.split("/")[0].toUpperCase());
     }
   };
   // useEffect(() => {
@@ -104,73 +109,71 @@ const MessageTypingBar: React.FC<MessageTypingBarProp> = ({
     setInput(e.target.value);
   };
 
-  const handleSendMessage = async() => {
-  try {
-    if(!id) return
+  const handleSendMessage = async () => {
+    try {
+      if (!id) return;
       // if (input.trim() === "" || !id ) return;
-  
-    if (ws && ws.readyState === WebSocket.OPEN) {
-      if(input.trim() !== ''){
-        let message: MessageProp = {
-          friendId: parseInt(id, 10),
-          data: input,
-          messageType: messageType,
-          createdAt: new Date(),
-        };
-        console.log('message', message);
-        ws.send(JSON.stringify(message));
-        onSendMessage(message);
-        setInput("");
 
+      if (ws && ws.readyState === WebSocket.OPEN) {
+        
+        if (input.trim() !== "") {
+          let message: MessageProp = {
+            friendId: parseInt(id, 10),
+            data: input,
+            messageType: messageType,
+            createdAt: new Date(),
+          };
+          console.log("message", message);
+          ws.send(JSON.stringify(message));
+          onSendMessage(message);
+          setInput("");
+        }
+        if (file) {
+          const reader = new FileReader();
+          reader.onloadend = async () => {
+            const base64data = reader.result as string;
+            const message: MessageProp = {
+              friendId: parseInt(id, 10),
+              data: base64data,
+              messageType: messageType,
+              contentType: fileData.type,
+              filename: filename,
+              createdAt: new Date(),
+            };
+            await uploadFile(file);
+            ws.send(JSON.stringify(message));
+            onSendMessage(message);
+            setFile(null);
+            setFileUrl("");
+          };
+          reader.readAsDataURL(file);
+        }
       }
-      if(file){
-        console.log(file);
-        // setFilename(`image-${Date.now()}.${file.type.split('/')[1]}`)
-        let message: MessageProp = {
-          friendId: parseInt(id, 10),
-          data: fileUrl,
-          messageType: messageType,
-          contentType : fileData.type,
-          filename: filename,
-          createdAt: new Date(),
-        };
-
-        await uploadFile(file)
-        console.log('message', message);
-        ws.send(JSON.stringify(message));
-        onSendMessage(message);
-        setInput("");
-        setFileUrl('')
-
-    
-      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error)
-  }
   };
 
   return (
-   
-      <div className="h-14 w-[90%] rounded-lg bg-white/15 flex relative">
-       <Media
+    <div className="h-14 w-[90%] rounded-lg bg-white/15 flex relative">
+      <Media
         handleDivClick={handleDivClick}
         handleFileChange={handleFileChange}
-        fileInputRef={fileInputRef} />
-        <input
-          className="bg-transparent
+        fileInputRef={fileInputRef}
+      />
+      <input
+        className="bg-transparent
         text-black outline-none
         w-[70%] placeholder:text-gray-700 px-4"
-          placeholder="Your Message"
-          value={input}
-          onChange={handleChange}
-        />
-        <div className="flex ">
-          <AudioRecorder />
-          <SendBtn handleSendMessage={handleSendMessage} />
-        </div>
+        placeholder="Your Message"
+        value={input}
+        onChange={handleChange}
+      />
+      <div className="flex ">
+        <AudioRecorder />
+        <SendBtn handleSendMessage={handleSendMessage} />
       </div>
-
+    </div>
   );
 };
 
