f38a5c0510413fe44bc6531094199cd9f38f69bc
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-25 22:17:47 +0530

    feat: add back button

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -1,22 +1,29 @@
+import { useSetRecoilState } from "recoil"
+import { User } from "../types/common"
 import SearchedUser from "./SearchedUser"
+import { searchInputAtom } from "../atom/atom"
 
-export interface User {
-    id: number
-    email: string
-    username: string
-}
 interface Users {
     users : User[]
 }
 const SearchModal : React.FC<Users> = ({users}) => {
+    const setSearchInput = useSetRecoilState(searchInputAtom)
 
   return (
-    <div className="absolute p-3 bg-black/80 overflow-y-scroll rounded-lg h-1/2 w-1/3 z-10  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="absolute py-3 px-5 bg-black/80  rounded-lg h-1/2 w-1/3 z-10  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+    flex items-center flex-col border ">
+        <div className="h-full w-full overflow-y-scroll">
         {
             users.map((user)=>{
                 return <SearchedUser user={user}/>
             })
         }
+        </div>
+       
+         <button 
+      onClick={()=>setSearchInput('')}
+      className="h-10 w-max py-1 my-2 px-5 bg-red-600 text-white rounded-lg
+      hover:bg-red-800 ">Close</button>
         </div>
   )
 }
