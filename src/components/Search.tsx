e2376e1e1aaca85dcc7f3cfc370efdeae72f0468
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-06-25 22:16:54 +0530

    refactor: add searchInputAtom state

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,37 +3,43 @@ import { FiSearch } from "react-icons/fi";
 import SearchModal from "./SearchModal";
 import axios from "axios";
 import { TOKEN } from "../utils/util";
+import { searchInputAtom } from "../atom/atom";
+import { useRecoilState } from "recoil";
 
 const Search = () => {
-  const [input , setInput] = useState('')
-  const [users ,setUsers] = useState([])
-  useEffect(()=>{
-    
-    axios.get(`${import.meta.env.VITE_BASE_URL}/user/search`,{
-      params:{
-        search: input
-      },
-      headers:{
-        Authorization: `Bearer ${TOKEN}`
-      }
-    }).then(res => setUsers(res.data.users))
-    console.log(users);
-  },[input])
+  const [searchInput, setSearchInput] = useRecoilState(searchInputAtom);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`/user/search`, {
+        params: {
+          search: searchInput,
+        },
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      })
+      .then((res) => setUsers(res.data.users));
+  }, [searchInput]);
+  
   return (
     <div className="h-10 w-full bg-white/15 border border-black flex text-white rounded-lg ">
-        <div className="h-10 w-10 text-2xl flex justify-center items-center text-black"><FiSearch /></div>
-        <input type="text" className="h-full w-full bg-transparent outline-none px-1
+      <div className="h-10 w-10 text-2xl flex justify-center items-center text-black">
+        <FiSearch />
+      </div>
+      <input
+        type="text"
+        className="h-full w-full bg-transparent outline-none px-1
         placeholder:text-black"
-        onChange={(e)=>{
-          setInput(e.target.value)
+        onChange={(e) => {
+          setSearchInput(e.target.value);
         }}
-        placeholder="search" />
-        {input.trim() !== '' && 
-        
-        <SearchModal users={users} />
-        }
+        value={searchInput}
+        placeholder="search"
+      />
+      {searchInput.trim() !== "" && <SearchModal users={users} />}
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
