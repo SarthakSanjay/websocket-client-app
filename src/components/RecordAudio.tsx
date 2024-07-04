dfaf5bd4fa7ceeabd6cc41b41df75dd57a44fd91
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-07-04 23:25:13 +0530

    feat: added a custom audio recorder

--- src/components/RecordAudio.tsx
+++ src/components/RecordAudio.tsx
@@ -1,68 +1,75 @@
-import React, { useEffect, useState } from "react";
+import {  useRef, useState } from "react";
 import { PiMicrophone, PiMicrophoneSlash } from "react-icons/pi";
 import { useSetRecoilState } from "recoil";
 import { toggleRecordAtom } from "../atom/atom";
 import { useReactMediaRecorder } from "react-media-recorder";
 import AudioPlayer from "./AudioPlayer";
-import { BiPause } from "react-icons/bi";
+import { BiPause, BiPlay } from "react-icons/bi";
 import { MdRestartAlt } from "react-icons/md";
 import { useStopwatch } from "react-timer-hook";
 
+
 const RecordAudio = () => {
   const [isRecording, setIsRecording] = useState(false);
-  // const [second , setSecond] = useState('00')
-  // const [minute , setMinute] = useState('00')
+  const [isPaused ,setIsPaused] = useState(false)
   const setToggleRecord = useSetRecoilState(toggleRecordAtom);
   const {
-    status,
     startRecording,
     stopRecording,
     pauseRecording,
     mediaBlobUrl,
     clearBlobUrl,
+    resumeRecording,
   } = useReactMediaRecorder({ video: false, audio: true });
-  const {start , pause , reset  , seconds , minutes , isRunning} = useStopwatch({autoStart: false})
-  console.log(isRecording);
-  console.log(mediaBlobUrl);
-
+  const { start, pause, reset, seconds, minutes, isRunning } = useStopwatch({
+    autoStart: false,
+  });
+  
   const startRecord = () => {
     setIsRecording(true);
     startRecording();
-    start()
+    start();
   };
 
   const stopRecord = () => {
     setIsRecording(false);
     stopRecording();
-    pause()
+    reset(undefined, false);
   };
 
   const restartRecord = () => {
     clearBlobUrl();
-    reset()
-    pause()
+    reset(undefined, false);
   };
 
-  const pauseRecord = () =>{
-    pauseRecording()
-    pause()
-  }
+  const pauseRecord = () => {
+    if(isPaused){
+      start()
+      resumeRecording()
+      setIsPaused(false)
+    }else{
+      pauseRecording();
+      pause();
+      setIsPaused(true)
+    }
+  };
+
   return (
     <div
       className="h-[400px] w-[500px] bg-black/85 rounded-lg
     absolute bottom-32 z-50 text-white flex justify-center items-center
     flex-col gap-4 "
     >
-      <div className="border">{status}</div>
-      {isRunning && <div className="border">{minutes}:{seconds}</div>}
-      {mediaBlobUrl ? <AudioPlayer src={mediaBlobUrl} /> : ""}
-      {isRecording ? "Stop when done" : "Start speaking"}
-
+      {mediaBlobUrl ? <AudioPlayer src={mediaBlobUrl} /> : <div className="h-16 p-2 text-[35px]">
+        {minutes < 10 && "0"}
+        {minutes}:{seconds < 10 && "0"}
+        {seconds}
+      </div>}
       <div className="flex gap-3">
         {isRecording ? (
           <button
             onClick={stopRecord}
-            className="h-14 w-14 rounded-full bg-orange-500 text-black
+            className="h-14 w-14 rounded-full bg-gradient-to-r from-indigo-500 to-blue-500 text-black
         flex justify-center items-center text-3xl hover:bg-sky-500 hover:text-white"
           >
             <PiMicrophoneSlash />
@@ -70,7 +77,7 @@ const RecordAudio = () => {
         ) : (
           <button
             onClick={startRecord}
-            className="h-14 w-14 rounded-full bg-green-500 text-black
+            className="h-14 w-14 rounded-full bg-gradient-to-r from-emerald-400 to-cyan-400 text-black
         flex justify-center items-center text-3xl hover:bg-sky-500 hover:text-white"
           >
             <PiMicrophone />
@@ -79,21 +86,21 @@ const RecordAudio = () => {
 
         <button
           onClick={pauseRecord}
-          className="h-14 w-14 rounded-full bg-white text-black
+          className="h-14 w-14 rounded-full bg-gradient-to-r from-purple-600 to-red-500 text-black
         flex justify-center items-center text-3xl hover:bg-sky-500 hover:text-white"
         >
-          <BiPause />
+          {isPaused ? <BiPlay /> :<BiPause />}
         </button>
 
         <button
           onClick={restartRecord}
-          className="h-14 w-14 rounded-full bg-yellow-400 text-black
+          className="h-14 w-14 rounded-full bg-gradient-to-r from-yellow-300 to-lime-500 text-black
         flex justify-center items-center text-3xl hover:bg-sky-500 hover:text-white"
         >
           <MdRestartAlt />
         </button>
       </div>
-
+      
       <button
         onClick={() => setToggleRecord((prev) => !prev)}
         className="h-10 w-max py-1 px-5 bg-red-600 text-white rounded-lg
