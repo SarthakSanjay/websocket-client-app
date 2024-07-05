5026ccf67f9eded4787c528d560d5048e1420321
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-07-05 23:26:48 +0530

    feat: add audioRecorder toggle

--- src/components/AudioRecorder.tsx
+++ src/components/AudioRecorder.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { BiMicrophone } from 'react-icons/bi'
+import { BiMicrophone } from "react-icons/bi";
+import { useSetRecoilState } from "recoil";
+import { toggleRecordAtom } from "../atom/atom";
 
 const AudioRecorder = () => {
-  return (
-    <div
-    className="h-14 w-14 text-2xl flex items-center
-  justify-center text-black rounded-full  hover:bg-black/35 hover:text-white transition-all duration-500"
-  >
-    <BiMicrophone />
-  </div>
-  )
-}
+  const setToggleRecord = useSetRecoilState(toggleRecordAtom);
 
-export default AudioRecorder
+  const handleClick = () => {
+    setToggleRecord((prev) => !prev);
+  };
+  
+  return (
+    <button
+      onClick={handleClick}
+      className="h-14 w-14 text-2xl flex items-center
+    justify-center text-black rounded-full 
+  hover:bg-black/35 hover:text-white transition-all duration-500"
+    >
+      <BiMicrophone />
+    </button>
+  );
+};
+
+export default AudioRecorder;
