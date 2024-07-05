9c2d90b490a060f9a5c30fe84f72ea579733e2f3
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-07-05 23:15:49 +0530

    refactor :did some cleanup

--- src/components/MessageTypingBar.tsx
+++ src/components/MessageTypingBar.tsx
@@ -1,12 +1,9 @@
-import { useParams } from "react-router-dom";
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import SendBtn from "./SendBtn";
 import AudioRecorder from "./AudioRecorder";
 import Media from "./Media";
-import { fileUrlAtom } from "../atom/atom";
-import { useRecoilState } from "recoil";
-import axios from "axios";
-import { TOKEN } from "../utils/util";
+import { fileAtom, fileUrlAtom, inputAtom, messageTypeAtom } from "../atom/atom";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 export interface MessageProp {
   friendId: number;
@@ -18,7 +15,7 @@ export interface MessageProp {
 }
 
 interface MessageTypingBarProp {
-  ws: WebSocket | null;
+  ws: WebSocket | null 
   onSendMessage: (message: MessageProp) => void;
 }
 
@@ -26,53 +23,14 @@ const MessageTypingBar: React.FC<MessageTypingBarProp> = ({
   ws,
   onSendMessage,
 }) => {
-  const { id } = useParams<{ id: string }>();
-  const [input, setInput] = useState("");
-  const [messageType, setMessageType] = useState("TEXT");
-  const [_fileUrl, setFileUrl] = useRecoilState(fileUrlAtom);
-  const [fileData, setFileData] = useState<{ name: string; type: string }>({
-    name: "",
-    type: "",
-  });
-  const [file, setFile] = useState(null);
-  const fileInputRef = useRef(null);
-  const [filename, _setFilename] = useState(`${Date.now()}`);
+  const setMessageType = useSetRecoilState(messageTypeAtom)
+  const setFileUrl = useSetRecoilState(fileUrlAtom);
+  const setFile = useSetRecoilState(fileAtom)
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [input , setInput] = useRecoilState(inputAtom)
 
-  async function uploadFile(file: any) {
-    const fileExtension = file.type.split("/")[1]
-    const fileName = `${filename}.${fileExtension}`;
-
-    try {
-      let res = await axios.post(
-        `/upload/file`,
-        {
-          filename: fileName,
-          contentType: file.type,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${TOKEN}`,
-          },
-        }
-      );
-      if (res.status === 200) {
-        let upload = await axios.put(res.data.url, file, {
-          headers: {
-            "Content-Type": file.type,
-          },
-        });
-
-        if (upload.status === 200) {
-          alert(`image uploaded ${upload.status} ${upload}`);
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
   const handleDivClick = () => {
     if (fileInputRef.current) {
-      //@ts-ignore
       fileInputRef.current.click();
     }
   };
@@ -84,71 +42,12 @@ const MessageTypingBar: React.FC<MessageTypingBarProp> = ({
     if (file) {
       const url = URL.createObjectURL(file);
       setFileUrl(url);
-      setFileData({ name: file.name, type: file.type });
       setMessageType(file.type.split("/")[0].toUpperCase());
     }
   };
-  // useEffect(() => {
-  //   const handleKeyDown = (e: KeyboardEvent) => {
-  //     if (e.key === "Enter") {
-  //       handleSendMessage();
-  //     }
-  //   };
-
-  //   window.addEventListener("keydown", handleKeyDown);
-
-  //   return () => {
-  //     window.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
-  };
-
-  const handleSendMessage = async () => {
-    try {
-      if (!id) return;
-      // if (input.trim() === "" || !id ) return;
-
-      if (ws && ws.readyState === WebSocket.OPEN) {
-        
-        if (input.trim() !== "") {
-          let message: MessageProp = {
-            friendId: parseInt(id, 10),
-            data: input,
-            messageType: messageType,
-            createdAt: new Date(),
-          };
-          console.log("message", message);
-          ws.send(JSON.stringify(message));
-          onSendMessage(message);
-          setInput("");
-        }
-        if (file) {
-          const reader = new FileReader();
-          reader.onloadend = async () => {
-            const base64data = reader.result as string;
-            const message: MessageProp = {
-              friendId: parseInt(id, 10),
-              data: base64data,
-              messageType: messageType,
-              contentType: fileData.type,
-              filename: filename,
-              createdAt: new Date(),
-            };
-            await uploadFile(file);
-            ws.send(JSON.stringify(message));
-            onSendMessage(message);
-            setFile(null);
-            setFileUrl("");
-          };
-          reader.readAsDataURL(file);
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
   };
 
   return (
@@ -168,7 +67,7 @@ const MessageTypingBar: React.FC<MessageTypingBarProp> = ({
       />
       <div className="flex ">
         <AudioRecorder />
-        <SendBtn handleSendMessage={handleSendMessage} />
+        <SendBtn ws={ws} onSendMessage={onSendMessage} />
       </div>
     </div>
   );
